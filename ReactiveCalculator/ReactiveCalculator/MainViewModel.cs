c55b8ba95c4e4808624fe1a1716719ed13fbeeba
Peter Samsonov <psamsonov@guestlogix.com>
2018-10-09 15:11:40 -0400

    Adding negate command

--- ReactiveCalculator/ReactiveCalculator/MainViewModel.cs
+++ ReactiveCalculator/ReactiveCalculator/MainViewModel.cs
@@ -14,13 +14,31 @@ namespace ReactiveCalculator
         public ICommand Append { get; private set; }
         public ICommand Clear { get; private set; }
         public ICommand Backspace { get; private set; }
+        public ICommand Negate { get; private set; }
 
         public MainViewModel()
         {
             Append = ReactiveCommand.Create<string>( (x) => { this.Number = Number += x; });
             Clear = ReactiveCommand.Create( () => this.Number = "0");
-            Backspace = ReactiveCommand.Create(() => this.Number = this.Number.Substring(0, this.Number.Length - 1)); 
+            Backspace = ReactiveCommand.Create(() => this.Number = this.Number.Substring(0, this.Number.Length - 1));
 
+            Negate = ReactiveCommand.Create(() =>
+                {
+
+                    if (this.Number == "0")
+                    {
+                        //Do nothing, no such thing as negative zero
+                    }
+                    else if (this.Number.StartsWith("-"))
+                    {
+                        this.Number = this.Number.Substring(1, this.Number.Length - 1);
+                    }
+                    else
+                    {
+                        this.Number = "-" + this.Number;
+                    }
+                }
+            );
 
             this.WhenAnyValue(x => x.Number).Subscribe(_ => ValidateNumber());
         }
