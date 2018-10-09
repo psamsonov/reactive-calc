261f1218c37f5b65631f54547ccedd712fa103e5
Peter Samsonov <psamsonov@guestlogix.com>
2018-10-09 11:35:25 -0400

    Adding a binding for the numeric value

---
+++ ReactiveCalculator/ReactiveCalculator/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ReactiveCalculator
+{
+    class MainViewModel : BindableObject
+    {
+        public static readonly BindableProperty NumberProperty = BindableProperty.Create(
+            nameof(Number),
+            typeof(string),
+            typeof(MainViewModel),
+            "0",
+            BindingMode.TwoWay,null, NumberChanged
+            
+        );
+
+        private static void NumberChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            var main = bindable as MainViewModel;
+            if (string.IsNullOrWhiteSpace(main.Number))
+                main.Number = "0";
+        }
+
+
+        public string Number
+        {
+            get => (string)this.GetValue(NumberProperty);
+            set => this.SetValue(NumberProperty,value); 
+        }
+    }
+}
